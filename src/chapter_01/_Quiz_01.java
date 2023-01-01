f4a998b53f8423cf3f5a9243e4eed265c31a358d
SJBANG <sjbang8098@gmail.com>
2023-01-01 16:15:01 +0900

    commit

---
+++ src/chapter_01/_Quiz_01.java
@@ -0,0 +1,20 @@
+package chapter_01;
+
+public class _Quiz_01 {
+    public static void main(String[] args) {
+        // 버스 번호 (1234, 상암08)
+        String no = "상암08";
+
+        // 남은 시간 (3분, 5분)
+        int min = 3;
+
+        // 남은 거리 (1.5km, 0.8km)
+        double distance = 1.5;
+
+        // 결과 출력
+        System.out.println(no + "번 버스");
+        System.out.println("약 " + min + "분 후 도착");
+        System.out.println("남은 거리 " + distance + "km");
+
+    }
+}
---
+++ src/chapter_02/_01_Operator1.java
@@ -0,0 +1,13 @@
+package chapter_02;
+
+public class _01_Operator1 {
+    public static void main(String[] args) {
+        // 산술 연산자
+
+        // 일반 연산
+        System.out.println(4 + 2); // 6
+        System.out.println(4 - 2); // 2
+        System.out.println(4 * 2); // 8
+
+    }
+}
